b0d3f53d45e53bc2a4558650324c35360b75b458
Alexandre <alexandre.viry@trust-in-soft.com>
2020-08-04 14:16:54 +0200

    [FIX] good values line 19

--- tests/tests_timeofday.c
+++ tests/tests_timeofday.c
@@ -19,16 +19,15 @@ int main(void)
     //@ assert tv.tv_sec == 1596453215 && tv.tv_usec == 1596453215000;
     res = gettimeofday(&tv, &tz);
     //@ assert res == 0;
-    //@ assert tv.tv_sec == 1596453215 && tv.tv_usec == 1596453215000;
-    //@ assert tz.tz_minuteswest == 26607553 && tz.tz_dsttime == 0;
-    res = gettimeofday(&tv2, &tz);
     //@ assert tv.tv_sec == 1596453275 && tv.tv_usec == 1596453275000;
-    //@ assert tz.tz_minuteswest == 26607554 && tz.tz_dsttime == 0;
-    //@ assert (tv2.tv_sec - tv.tv_sec) == 60;
+    //@ assert tz.tz_minuteswest == 26607554 && tz.tz_dsttime == 0;    
     res = gettimeofday(&tv2, &tz);
     //@ assert tv.tv_sec == 1596453335 && tv.tv_usec == 1596453335000;
     //@ assert tz.tz_minuteswest == 26607555 && tz.tz_dsttime == 0;
-    //@ assert (tv2.tv_sec - tv.tv_sec) == 120;
+    res = gettimeofday(&tv2, &tz);
+    //@ assert tv.tv_sec == 1596453395 && tv.tv_usec == 1596453395000;
+    //@ assert tz.tz_minuteswest == 26607556 && tz.tz_dsttime == 0;
+
 
     res = settimeofday(NULL, NULL);
     //@ assert res == -1;
--- tests/tests_timeofday2.c
+++ tests/tests_timeofday2.c
@@ -17,17 +17,14 @@ int main(void)
     //@ assert res == 0;
     //@ assert tv.tv_sec == 4718517215 && tv.tv_usec == 4718517215000;
     res = gettimeofday(&tv, &tz);
-    //@ assert res == 0;
-    //@ assert tv.tv_sec == 4718517215 && tv.tv_usec == 4718517215000;
-    //@ assert tz.tz_minuteswest == 78641953 && tz.tz_dsttime == 0;
-    res = gettimeofday(&tv2, &tz);
     //@ assert tv.tv_sec == 4718603615 && tv.tv_usec == 4718603615000;
     //@ assert tz.tz_minuteswest == 78643393 && tz.tz_dsttime == 0;
-    //@ assert (tv2.tv_sec - tv.tv_sec) == 86400;
     res = gettimeofday(&tv2, &tz);
     //@ assert tv.tv_sec == 4718690015 && tv.tv_usec == 4718690015000;
     //@ assert tz.tz_minuteswest == 78644833 && tz.tz_dsttime == 0;
-    //@ assert (tv2.tv_sec - tv.tv_sec) == 172800;
+    res = gettimeofday(&tv2, &tz);
+    //@ assert tv.tv_sec == 4718776415 && tv.tv_usec == 4718776415000;
+    //@ assert tz.tz_minuteswest == 78646273 && tz.tz_dsttime == 0;
 
     res = settimeofday(NULL, NULL);
     //@ assert res == -1;
