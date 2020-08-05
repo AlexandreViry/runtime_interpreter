2e05d9d3742c849788c74ede8738611ecec300b6
Alexandre <alexandre.viry@trust-in-soft.com>
2020-08-05 14:20:48 +0200

    [ADD] tm structure and two tests

--- tis_runtime_interpreter.c
+++ tis_runtime_interpreter.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <sys/time.h>
+#include <time.h>
+
+// Gettimeofday and settimeofday Macros :
 
 #ifndef TIS_INIT_TIME
 // Number of elapsed seconds on August 3, 2020.
@@ -10,6 +13,14 @@
 #define TIS_INCR_TIME 60L
 #endif
 
+// My_mktime Macros :
+
+#ifndef INIT_VALUE
+#define INIT_VALUE 1000L
+#endif
+#ifndef INCR_VALUE
+#define INCR_VALUE 1000L
+#endif
 
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
@@ -39,4 +50,31 @@ int settimeofday(const struct timeval *tv, const struct timezone *tz)
 	return -1;
 
     return 0;
+}
+
+time_t my_mktime(struct tm *time)
+{
+    static long res = INIT_VALUE;
+
+    if (time == NULL)
+        return -1;
+    res += INCR_VALUE;
+    return res;
+}
+
+time_t time_structure_modifier(struct tm *time)
+{
+    long res = 0;
+
+    if (time == NULL)
+        return -1;
+    res = time->tm_sec;
+    res += time->tm_min * 100;
+    res += time->tm_hour * 10000;
+    res += time->tm_year * 1000000;
+    res += time->tm_mon * 10000000000;
+    res += time->tm_mday * 1000000000000;
+    res += time->tm_wday * 100000000000000;
+    res += time->tm_yday * 1000000000000000;
+    return res;
 }
