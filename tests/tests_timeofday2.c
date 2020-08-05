d30d85adfec42f01b3130e7eca45caa762a35242
Alexandre <alexandre.viry@trust-in-soft.com>
2020-08-05 09:08:44 +0200

    Tz update

--- tests/tests_timeofday2.c
+++ tests/tests_timeofday2.c
@@ -15,7 +15,7 @@ int main(void)
     //@ assert tz.tz_minuteswest == 78641953 && tz.tz_dsttime == 0;
     res = gettimeofday(&tv, NULL);
     //@ assert res == 0;
-    //@ assert tz.tz_minuteswest == 78644833 && tz.tz_dsttime == 0;
+    //@ assert tz.tz_minuteswest == 78641953 && tz.tz_dsttime == 0;
     //@ assert tv.tv_sec == 4718603615 && tv.tv_usec == 4718603615000;
     res = gettimeofday(&tv, &tz);
     //@ assert tv.tv_sec == 4718690015 && tv.tv_usec == 4718690015000;
