c8e25d403b95396a69d67753b6f9447fc6632a4a
Alexandre <alexandre.viry@trust-in-soft.com>
2020-08-06 17:43:11 +0200

    [FIX] tis.config

--- tests/tests_timeofday2.c
+++ tests/tests_timeofday2.c
@@ -18,13 +18,13 @@ int main(void)
     //@ assert tv.tv_sec == 4718603615 && tv.tv_usec == 4718603615000;
     res = gettimeofday(&tv, &tz);
     //@ assert tv.tv_sec == 4718690015 && tv.tv_usec == 4718690015000;
-    //@ assert tz.tz_minuteswest == 78646273 && tz.tz_dsttime == 0;
+    //@ assert tz.tz_minuteswest == 78644833 && tz.tz_dsttime == 0;
     res = gettimeofday(&tv, &tz);
     //@ assert tv.tv_sec == 4718776415 && tv.tv_usec == 4718776415000;
-    //@ assert tz.tz_minuteswest == 78647713 && tz.tz_dsttime == 0;
+    //@ assert tz.tz_minuteswest == 78646273 && tz.tz_dsttime == 0;
     res = gettimeofday(&tv, &tz);
     //@ assert tv.tv_sec == 4718862815 && tv.tv_usec == 4718862815000;
-    //@ assert tz.tz_minuteswest == 78649153 && tz.tz_dsttime == 0;
+    //@ assert tz.tz_minuteswest == 78647713 && tz.tz_dsttime == 0;
 
     res = settimeofday(NULL, NULL);
     //@ assert res == -1;
