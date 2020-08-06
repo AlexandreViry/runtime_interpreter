cc3999067bbf62be596c3db483e983a4c7de923a
Alexandre <alexandre.viry@trust-in-soft.com>
2020-08-06 16:23:11 +0200

    [FIX]

--- tis_runtime_interpreter.c
+++ tis_runtime_interpreter.c
@@ -24,6 +24,7 @@ struct timezone tis_internal_timezone = {
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
     static int count = 0;
+    printf("%d\n", count);
     if (tv == NULL && tz == NULL)
         return -1;
 
